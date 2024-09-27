ef7b124e8c7e65476da1217e02377ad9b323b8aa
Itskoketso <itskoketso@gmail.com>
2024-09-27 13:07:13 +0200

    Updated js to include current timezone

--- src/index.js
+++ src/index.js
@@ -11,21 +11,23 @@ function updateTime() {
     );
   }
 
-  let johannesburgElement = document.querySelector("#johannesburg");
-  if (johannesburgElement) {
+  let parisElement = document.querySelector("#paris");
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+
+    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format(
+      "h:mm:ss[<small>]A[</small>]"
+    );
   }
-  let johannesburgDateElement = johannesburgElement.querySelector(".date");
-  let johannesburgTimeElement = johannesburgElement.querySelector(".time");
-  let johannesburgTime = moment().tz("Africa/Johannesburg");
-
-  johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
-  johannesburgTimeElement.innerHTML = johannesburgTime.format(
-    "h:mm:ss[<small>]A[</small>]"
-  );
 }
-
 function updateCity(event) {
   let cityTZ = event.target.value;
+  if (cityTZ === "current") {
+    cityTZ = moment().tz.guess();
+  }
   let cityName = cityTZ.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTZ);
   let citiesElement = document.querySelector("#cities");
