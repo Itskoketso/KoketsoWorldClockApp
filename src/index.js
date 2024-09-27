ee5ac53e6b8529327f8fe6716980d5fb13f9c5be
Itskoketso <itskoketso@gmail.com>
2024-09-27 12:48:26 +0200

    Added function to update to city selected in dropdown, to js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 function updateTime() {
   let laElement = document.querySelector("#los-angeles");
-  let laDateElement = laElement.querySelector(".date");
-  let laTimeElement = laElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (laElement) {
+    let laDateElement = laElement.querySelector(".date");
+    let laTimeElement = laElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  laDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  laTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss[<small>]A[</small>]"
-  );
+    laDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    laTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss[<small>]A[</small>]"
+    );
+  }
 
   let johannesburgElement = document.querySelector("#johannesburg");
+  if (johannesburgElement) {
+  }
   let johannesburgDateElement = johannesburgElement.querySelector(".date");
   let johannesburgTimeElement = johannesburgElement.querySelector(".time");
   let johannesburgTime = moment().tz("Africa/Johannesburg");
@@ -19,5 +23,26 @@ function updateTime() {
     "h:mm:ss[<small>]A[</small>]"
   );
 }
+
+function updateCity(event) {
+  let cityTZ = event.target.value;
+  let cityName = cityTZ.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTZ);
+  let citiesElement = document.querySelector("#cities");
+
+  citiesElement.innerHTML = ` <div class="city" id= "los-angeles">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss[<small>]A[</small>]"
+        )}</div>
+      </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
